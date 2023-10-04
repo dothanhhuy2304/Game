a844b7ad5dcd1ef50b3fda1c8ba9d8e625407f25
dothanhhuy0189x@gmail.com <dothanhhuy0189x@gmail.com>
2023-10-04 12:16:08 +0700

    dohuy make game run in pc and mobile device

--- Assets/Script/Core/MobileInputManager.cs
+++ Assets/Script/Core/MobileInputManager.cs
@@ -5,4 +5,15 @@ public class MobileInputManager : MonoBehaviour
     public VariableJoystick joystick;
     public UnityEngine.UI.Button btnShot;
     public UnityEngine.UI.Button btnDash;
+    [SerializeField] private GameObject objMobileInput;
+
+
+    private void Awake()
+    {
+#if UNITY_STANDALONE
+        objMobileInput.SetActive(false);
+#elif UNITY_ANDROID|| UNITY_IOS
+        objMobileInput.SetActive(true);
+#endif
+    }
 }
